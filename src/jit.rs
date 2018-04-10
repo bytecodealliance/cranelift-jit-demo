d0ff80f4ddddcbad575dc1f5134d33d3910386c1
Dan Gohman <sunfish@mozilla.com>
2018-04-09 23:35:52 -0700

    Add an iterative_fib example, and add lots more comments.

--- src/jit.rs
+++ src/jit.rs
@@ -6,9 +6,10 @@ use cretonne::ir::types;
 use cretonne::ir::condcodes::IntCC;
 use cretonne;
 use cton_frontend::{FunctionBuilderContext, FunctionBuilder, Variable};
-use cton_module;
-use cton_simplejit;
+use cton_module::{Module, Linkage};
+use cton_simplejit::SimpleJITBackend;
 
+/// The AST node for expressions.
 pub enum Expr {
     Literal(String),
     Identifier(String),
@@ -23,81 +24,130 @@ pub enum Expr {
     Sub(Box<Expr>, Box<Expr>),
     Mul(Box<Expr>, Box<Expr>),
     Div(Box<Expr>, Box<Expr>),
-    If(Box<Expr>, Vec<Expr>, Vec<Expr>),
+    IfElse(Box<Expr>, Vec<Expr>, Vec<Expr>),
+    WhileLoop(Box<Expr>, Vec<Expr>),
     Call(String, Vec<Expr>),
 }
 
+/// Include the parser code, generated from grammar.rustpeg.
 mod parser {
     include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
 }
 
+/// The basic JIT class.
 pub struct JIT {
+    /// The function builder context, which is reused across multiple
+    /// FunctionBuilder instances.
     builder_context: FunctionBuilderContext<Variable>,
+
+    /// The main Cretonne context, which holds the state for codegen.
     ctx: cretonne::Context,
-    module: cton_module::Module<cton_simplejit::SimpleJITBackend>,
+
+    /// The module, with the simplejit backend, which manages the JIT'd
+    /// functions.
+    module: Module<SimpleJITBackend>,
 }
 
 impl JIT {
+    /// Create a new `JIT` instance.
     pub fn new() -> Self {
-        let backend = cton_simplejit::SimpleJITBackend::new();
+        let backend = SimpleJITBackend::new();
         Self {
             builder_context: FunctionBuilderContext::<Variable>::new(),
             ctx: cretonne::Context::new(),
-            module: cton_module::Module::new(backend),
+            module: Module::new(backend),
         }
     }
 
+    /// Compile a string in the toy language into machine code.
     pub fn compile(&mut self, input: &str) -> Result<*const u8, String> {
+        // First, parse the string, producing AST nodes.
         let (name, params, the_return, stmts) =
             parser::function(&input).map_err(|e| e.to_string())?;
 
+        // Then, translate the AST nodes into Cretonne IR.
         self.translate(params, the_return, stmts).map_err(
             |e| e.to_string(),
         )?;
+
+        // Next, declare the function to simplejit. Functions must be declared
+        // before they can be called, or defined.
+        //
+        // TODO: This may be an area where the API should be streamlined; should
+        // we have a version of `declare_function` that automatically declares
+        // the function?
         let id = self.module
-            .declare_function(
-                &name,
-                cton_module::Linkage::Export,
-                &self.ctx.func.signature,
-            )
+            .declare_function(&name, Linkage::Export, &self.ctx.func.signature)
             .map_err(|e| e.to_string())?;
+
+        // Define the function to simplejit. This finishes compilation, although
+        // there may be outstanding relocations to perform. Currently, simplejit
+        // cannot finish relocations until all functions to be called are
+        // defined. For this toy demo for now, we'll just finalize the function
+        // below.
         self.module.define_function(id, &mut self.ctx).map_err(
             |e| {
                 e.to_string()
             },
         )?;
+
+        // Now that compilation is finished, we can clear out the context state.
         self.ctx.clear();
+
+        // Finalize the function, finishing any outstanding relocations. The
+        // result is a pointer to the finished machine code.
         let code = self.module.finalize_function(id);
 
         Ok(code)
     }
 
+    // Translate from toy-language AST nodes into Cretonne IR.
     fn translate(
         &mut self,
         params: Vec<String>,
         the_return: String,
         stmts: Vec<Expr>,
     ) -> Result<(), String> {
+        // Our toy language currently only supports I32 values, though Cretonne
+        // supports other types.
         for _p in &params {
             self.ctx.func.signature.params.push(
                 AbiParam::new(types::I32),
             );
         }
+
+        // Our toy language currently only supports one return value, though
+        // Cretonne is designed to support more.
         self.ctx.func.signature.returns.push(
             AbiParam::new(types::I32),
         );
 
+        // Create the builder to builder a function.
         let mut builder =
             FunctionBuilder::<Variable>::new(&mut self.ctx.func, &mut self.builder_context);
 
-        // TODO: Streamline the API here.
+        // Create the entry block, to start emitting code in.
         let entry_ebb = builder.create_ebb();
+
+        // Since this is the entry block, add block parameters corresponding to
+        // the function's parameters.
+        //
+        // TODO: Streamline the API here.
         builder.append_ebb_params_for_function_params(entry_ebb);
+
+        // Tell the builder to emit code in this block.
         builder.switch_to_block(entry_ebb);
+
+        // And, tell the builder that this block will have no further
+        // predecessors. Since it's the entry block, it won't have any
+        // predecessors.
         builder.seal_block(entry_ebb);
 
+        // The toy language allows variables to be declared implicitly.
+        // Walk the AST and declare all implicitly-declared variables.
         let variables = declare_variables(&mut builder, &params, &the_return, &stmts, entry_ebb);
 
+        // Now translate the statements of the function body.
         let mut trans = FunctionTranslator {
             builder,
             variables,
@@ -106,24 +156,33 @@ impl JIT {
         for expr in stmts {
             trans.translate_expr(expr);
         }
+
+        // Set up the return variable of the function. Above, we declared a
+        // variable to hold the return value. Here, we just do a use of that
+        // variable.
         let return_variable = trans.variables.get(&the_return).unwrap();
         let return_value = trans.builder.use_var(*return_variable);
+
+        // Emit the return instruction.
         trans.builder.ins().return_(&[return_value]);
 
+        // Tell the builder we're done with this function.
         trans.builder.finalize();
         Ok(())
     }
 }
 
+/// A collection of state used for translating from toy-language AST nodes
+/// into Cretonne IR.
 struct FunctionTranslator<'a> {
     builder: FunctionBuilder<'a, Variable>,
     variables: HashMap<String, Variable>,
-    module: &'a mut cton_module::Module<cton_simplejit::SimpleJITBackend>,
+    module: &'a mut Module<SimpleJITBackend>,
 }
 
 impl<'a> FunctionTranslator<'a> {
-    // When you write out instructions in Cretonne, you get back `Value`s. You
-    // can then use these references in other instructions.
+    /// When you write out instructions in Cretonne, you get back `Value`s. You
+    /// can then use these references in other instructions.
     fn translate_expr(&mut self, expr: Expr) -> Value {
         match expr {
             Expr::Literal(literal) => {
@@ -208,30 +267,43 @@ impl<'a> FunctionTranslator<'a> {
             Expr::Call(name, args) => self.translate_call(name, args),
 
             Expr::Identifier(name) => {
+                // `use_var` is used to read the value of a variable.
                 let variable = self.variables.get(&name).unwrap();
                 self.builder.use_var(*variable)
             }
 
             Expr::Assign(name, expr) => {
+                // `def_var` is used to write the value of a variable. Note that
+                // variables can have multiple definitions. Cretonne will
+                // convert them into SSA form for itself automatically.
                 let new_value = self.translate_expr(*expr);
                 let variable = self.variables.get(&name).unwrap();
                 self.builder.def_var(*variable, new_value);
                 new_value
             }
 
-            Expr::If(condition, then_body, else_body) => {
+            Expr::IfElse(condition, then_body, else_body) => {
                 let condition_value = self.translate_expr(*condition);
 
                 let else_block = self.builder.create_ebb();
                 let merge_block = self.builder.create_ebb();
+
+                // If-else constructs in the toy language have a return value.
+                // In traditional SSA form, this would produce a PHI between
+                // the then and else bodies. Cretonne uses block parameters,
+                // so set up a parameter in the merge block, and we'll pass
+                // the return values to it from the branches.
                 self.builder.append_ebb_param(merge_block, types::I32);
 
+                // Test the if condition and conditionally branch.
                 self.builder.ins().brz(condition_value, else_block, &[]);
 
                 let mut then_return = self.builder.ins().iconst(types::I32, 0);
                 for expr in then_body {
                     then_return = self.translate_expr(expr);
                 }
+
+                // Jump to the merge block, passing it the block return value.
                 self.builder.ins().jump(merge_block, &[then_return]);
 
                 self.builder.switch_to_block(else_block);
@@ -240,13 +312,46 @@ impl<'a> FunctionTranslator<'a> {
                 for expr in else_body {
                     else_return = self.translate_expr(expr);
                 }
+
+                // Jump to the merge block, passing it the block return value.
                 self.builder.ins().jump(merge_block, &[else_return]);
 
+                // Switch to the merge block for subsequent statements.
                 self.builder.switch_to_block(merge_block);
+
+                // We've now seen all the predecessors of the merge block.
                 self.builder.seal_block(merge_block);
+
+                // Read the value of the if-else by reading the merge block
+                // parameter.
                 let phi = self.builder.ebb_params(merge_block)[0];
 
                 phi
+            }
+
+            Expr::WhileLoop(condition, loop_body) => {
+                let header_block = self.builder.create_ebb();
+                let exit_block = self.builder.create_ebb();
+                self.builder.ins().jump(header_block, &[]);
+                self.builder.switch_to_block(header_block);
+
+                let condition_value = self.translate_expr(*condition);
+                self.builder.ins().brz(condition_value, exit_block, &[]);
+
+                for expr in loop_body {
+                    self.translate_expr(expr);
+                }
+                self.builder.ins().jump(header_block, &[]);
+
+                self.builder.switch_to_block(exit_block);
+
+                // We've reached the bottom of the loop, so there will be no
+                // more backedges to the header to exits to the bottom.
+                self.builder.seal_block(header_block);
+                self.builder.seal_block(exit_block);
+
+                // Just return 0 for now.
+                self.builder.ins().iconst(types::I32, 0)
             }
         }
     }
@@ -264,7 +369,7 @@ impl<'a> FunctionTranslator<'a> {
 
         // TODO: Streamline the API here?
         let callee = self.module
-            .declare_function(&name, cton_module::Linkage::Export, &sig)
+            .declare_function(&name, Linkage::Export, &sig)
             .expect("problem declaring function");
         let local_callee = self.module.declare_func_in_func(
             callee,
@@ -301,12 +406,39 @@ fn declare_variables(
     let return_variable = declare_variable(builder, &mut variables, &mut index, the_return);
     builder.def_var(return_variable, zero);
     for expr in stmts {
-        if let Expr::Assign(ref name, _) = *expr {
-            declare_variable(builder, &mut variables, &mut index, name);
-        }
+        declare_variables_in_stmt(builder, &mut variables, &mut index, expr);
     }
 
     variables
+}
+
+/// Recursively descend through the AST, translating all implicit
+/// variable declarations.
+fn declare_variables_in_stmt(
+    builder: &mut FunctionBuilder<Variable>,
+    variables: &mut HashMap<String, Variable>,
+    index: &mut usize,
+    expr: &Expr,
+) {
+    match *expr {
+        Expr::Assign(ref name, _) => {
+            declare_variable(builder, variables, index, name);
+        }
+        Expr::IfElse(ref _condition, ref then_body, ref else_body) => {
+            for stmt in then_body {
+                declare_variables_in_stmt(builder, variables, index, &stmt);
+            }
+            for stmt in else_body {
+                declare_variables_in_stmt(builder, variables, index, &stmt);
+            }
+        }
+        Expr::WhileLoop(ref _condition, ref loop_body) => {
+            for stmt in loop_body {
+                declare_variables_in_stmt(builder, variables, index, &stmt);
+            }
+        }
+        _ => (),
+    }
 }
 
 fn declare_variable(
--- src/toy.rs
+++ src/toy.rs
@@ -9,10 +9,17 @@ use std::mem;
 mod jit;
 
 fn main() {
+    // Create the JIT instance, which manages all generated functions and data.
     let mut jit = jit::JIT::new();
 
+    // A small test function.
+    //
+    // The `(c)` declares a return variable; the function returns whatever value
+    // it was assigned when the function exits. Note that there are multiple
+    // assignments, so the input is not in SSA form, but that's ok because
+    // Cretonne handles all the details of translating into SSA form itself.
     let foo_code = "\
-        fn foo(a, b) -> (d) {      \n\
+        fn foo(a, b) -> (c) {      \n\
             c = if a {             \n\
                 if b {             \n\
                     30             \n\
@@ -22,21 +29,30 @@ fn main() {
             } else {               \n\
                 50                 \n\
             }                      \n\
-            d = c + 2              \n\
+            c = c + 2              \n\
         }                          \n\
     ";
 
+    // Pass the string to the JIT, and it returns a raw pointer to machine code.
     let foo = jit.compile(&foo_code).unwrap_or_else(|msg| {
         eprintln!("error: {}", msg);
         process::exit(1);
     });
 
+    // Cast the raw pointer to a typed function pointer. This is unsafe, because
+    // this is the critical point where you have to trust that the generated code
+    // is safe to be called.
+    //
     // TODO: Is there a way to fold this transmute into `compile` above?
     let foo = unsafe { mem::transmute::<_, fn(i32, i32) -> i32>(foo) };
 
+    // And now we can call it!
     println!("the answer is: {}", foo(1, 0));
 
 
+    // -------------------------------------------------------------------------//
+
+    // Another example: Recursive fibonacci.
     let recursive_fib_code = "\
         fn recursive_fib(n) -> (r) {                                      \n\
             r = if n == 0 {                                               \n\
@@ -51,12 +67,45 @@ fn main() {
         }                                                                 \n\
     ";
 
+    // Same as above.
     let recursive_fib = jit.compile(&recursive_fib_code).unwrap_or_else(|msg| {
         eprintln!("error: {}", msg);
         process::exit(1);
     });
-
     let recursive_fib = unsafe { mem::transmute::<_, fn(i32) -> i32>(recursive_fib) };
 
+    // And we can now call it!
     println!("recursive_fib(10) = {}", recursive_fib(10));
+
+
+    // -------------------------------------------------------------------------//
+
+    // Another example: Iterative fibonacci.
+    let iterative_fib_code = "\
+        fn iterative_fib(n) -> (r) {                                      \n\
+            if n == 0 {                                                   \n\
+                r = 0                                                     \n\
+            } else {                                                      \n\
+                n = n - 1                                                 \n\
+                a = 0                                                     \n\
+                r = 1                                                     \n\
+                while n != 0 {                                            \n\
+                    t = r                                                 \n\
+                    r = r + a                                             \n\
+                    a = t                                                 \n\
+                    n = n - 1                                             \n\
+                }                                                         \n\
+            }                                                             \n\
+        }                                                                 \n\
+    ";
+
+    // Same as above.
+    let iterative_fib = jit.compile(&iterative_fib_code).unwrap_or_else(|msg| {
+        eprintln!("error: {}", msg);
+        process::exit(1);
+    });
+    let iterative_fib = unsafe { mem::transmute::<_, fn(i32) -> i32>(iterative_fib) };
+
+    // And we can now call it!
+    println!("iterative_fib(10) = {}", iterative_fib(10));
 }
