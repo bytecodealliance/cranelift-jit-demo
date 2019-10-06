8eee0bbfc8b3d9bd92d5fab8177930cc5d21d92c
Kevin Mehall <km@kevinmehall.net>
2019-10-06 08:39:16 -0700

    Update to rust-peg 0.6

--- build.rs
+++
@@ -1,5 +0,0 @@
-extern crate peg;
-
-fn main() {
-    peg::cargo_build("src/grammar.rustpeg");
-}
--- src/frontend.rs
+++ src/frontend.rs
@@ -19,7 +19,71 @@ pub enum Expr {
     GlobalDataAddr(String),
 }
 
-/// Include the parser code, generated from grammar.rustpeg.
-pub mod parser {
-    include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
-}
+peg::parser!(pub grammar parser() for str {
+    pub rule function() -> (String, Vec<String>, String, Vec<Expr>)
+        = [' ' | '\t' | '\n']* "fn" _ name:identifier() _
+        "(" params:((_ i:identifier() _ {i}) ** ",") ")" _
+        "->" _
+        "(" returns:(_ i:identifier() _ {i}) ")" _
+        "{" _ "\n"
+        stmts:statements()
+        _ "}" _ "\n" _
+        { (name, params, returns, stmts) }
+
+    rule statements() -> Vec<Expr>
+        = s:(statement()*) { s }
+
+    rule statement() -> Expr
+        = _ e:expression() _ "\n" { e }
+
+    rule expression() -> Expr
+        = if_else()
+        / while_loop()
+        / i:identifier() _ "=" _ e:expression() { Expr::Assign(i, Box::new(e)) }
+        / compare()
+
+    rule if_else() -> Expr
+        = "if" _ e:expression() _ "{" _ "\n"
+        then_body:statements() _ "}" _ "else" _ "{" _ "\n"
+        else_body:statements() _ "}"
+        { Expr::IfElse(Box::new(e), then_body, else_body) }
+
+    rule while_loop() -> Expr
+        = "while" _ e:expression() _ "{" _ "\n"
+        loop_body:statements() _ "}"
+        { Expr::WhileLoop(Box::new(e), loop_body) }
+
+    rule compare() -> Expr
+        = a:sum() _ "==" _ b:compare() { Expr::Eq(Box::new(a), Box::new(b)) }
+        / a:sum() _ "!=" _ b:compare() { Expr::Ne(Box::new(a), Box::new(b)) }
+        / a:sum() _ "<"  _ b:compare() { Expr::Lt(Box::new(a), Box::new(b)) }
+        / a:sum() _ "<=" _ b:compare() { Expr::Le(Box::new(a), Box::new(b)) }
+        / a:sum() _ ">"  _ b:compare() { Expr::Gt(Box::new(a), Box::new(b)) }
+        / a:sum() _ ">=" _ b:compare() { Expr::Ge(Box::new(a), Box::new(b)) }
+        / sum()
+
+    rule sum() -> Expr
+        = a:product() _ "+" _ b:sum() { Expr::Add(Box::new(a), Box::new(b)) }
+        / a:product() _ "-" _ b:sum() { Expr::Sub(Box::new(a), Box::new(b)) }
+        / product()
+
+    rule product() -> Expr
+        = a:call_or_identifier_or_literal() _ "*" _ b:product() { Expr::Mul(Box::new(a), Box::new(b)) }
+        / a:call_or_identifier_or_literal() _ "/" _ b:product() { Expr::Div(Box::new(a), Box::new(b)) }
+        / call_or_identifier_or_literal()
+
+    rule call_or_identifier_or_literal() -> Expr
+        = i:identifier() _ "(" args:((_ e:expression() _ {e}) ** ",") ")" { Expr::Call(i, args) }
+        / i:identifier() { Expr::Identifier(i) }
+        / literal()
+
+    rule identifier() -> String
+        = quiet!{ n:$(['a'..='z' | 'A'..='Z' | '_']['a'..='z' | 'A'..='Z' | '0'..='9' | '_']*) { n.to_owned() } }
+        / expected!("identifier")
+
+    rule literal() -> Expr
+        = n:$(['0'..='9']+) { Expr::Literal(n.to_owned()) }
+        / "&" i:identifier() { Expr::GlobalDataAddr(i) }
+
+    rule _() =  quiet!{[' ' | '\t']*}
+});
